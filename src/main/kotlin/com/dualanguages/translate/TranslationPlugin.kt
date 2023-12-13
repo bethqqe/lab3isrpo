fd57342d406a3c9b27e2b9e50dc3b57cf8d33224
beth <102821957+bethqqe@users.noreply.github.com>
2023-12-13 15:55:56 +0400

    Update TranslationPlugin.kt
    the correct output is separated by a space in deepl

--- src/main/kotlin/com/dualanguages/translate/TranslationPlugin.kt
+++ src/main/kotlin/com/dualanguages/translate/TranslationPlugin.kt
@@ -5,8 +5,6 @@ import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.Messages
 import com.intellij.openapi.actionSystem.CommonDataKeys
-import com.intellij.openapi.editor.Editor
-import com.intellij.openapi.editor.SelectionModel
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.application.ModalityState
 import java.awt.Desktop
@@ -19,29 +17,29 @@ class TranslationPlugin : AnAction() {
         val project = e.project
         val editor = e.getData(CommonDataKeys.EDITOR)
 
-        if (project != null && editor != null) {
-            val selectedText = getSelectedText(editor)
-
-            if (selectedText.isNotEmpty()) {
-                translateAndOpenBrowser(project, selectedText)
+        if (project != null) {
+            val textToTranslate = if (editor != null && editor.selectionModel.hasSelection()) {
+                editor.selectionModel.selectedText ?: ""
             } else {
-                showError(project, "No text selected", "Custom Error Title")
+                showInputPopup(project)
+            }
+
+            if (textToTranslate.isNotEmpty()) {
+                translateAndOpenBrowser(project, textToTranslate)
+            } else {
+                showError(project, "No text to translate", "Custom Error Title")
             }
         }
     }
 
-    private fun getSelectedText(editor: Editor): String {
-        val selectionModel: SelectionModel = editor.selectionModel
-        return if (selectionModel.hasSelection()) {
-            selectionModel.selectedText ?: ""
-        } else {
-            ""
-        }
+    private fun showInputPopup(project: Project): String {
+        return Messages.showInputDialog(project, "Enter text to translate:", "Text Translation", Messages.getQuestionIcon())
+            ?: ""
     }
 
     private fun translateAndOpenBrowser(project: Project, textToTranslate: String) {
         try {
-            val encodedText = URLEncoder.encode(textToTranslate, "UTF-8")
+            val encodedText = URLEncoder.encode(textToTranslate, "UTF-8").replace("+", "%20")
             val translationUrl = "https://www.deepl.com/translator#en/ru/$encodedText"
             openBrowser(translationUrl)
         } catch (e: Exception) {
